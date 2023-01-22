b163a7ab1d8fd8f510de05e3410cdf3525b4e0d4
phareebo <103385937+phareebo@users.noreply.github.com>
2023-01-22 19:52:55 +0300

    First commit

---
+++ PP_1_1_3-4_JDBC_HibernateTest/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,55 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+
+    }
+
+    public void createUsersTable() {
+        Connection connection = null;
+        try { connection = Util.createConnection();
+            Statement statement = connection.createStatement();
+            String SQL = "CREATE  TABLE IF NOT EXISTS usersTable(id INT AUTO_INCREMENT," +
+                    "name VARCHAR(50), lastName VARCHAR (50), age INT not NULL, PRIMARY KEY (id));";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void dropUsersTable() {
+
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+
+    }
+
+    public void removeUserById(long id) {
+
+    }
+
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    public void cleanUsersTable() {
+
+    }
+}
