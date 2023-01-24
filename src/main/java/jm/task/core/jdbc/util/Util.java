d0075a29d3202db649c881ff1edfe2868113f777
phareebo <103385937+phareebo@users.noreply.github.com>
2023-01-24 14:43:36 +0300

    try-with-resources used

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,35 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    public Util() {
+    }
+
+    // настройка соеденения с Базой Данных
+
+    private static final String URL = "jdbc:mysql://localhost:3306/pp1";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Romawka123!";
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
+}
